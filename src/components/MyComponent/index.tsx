bc8ccab640b00b42f8284d01b0b7564688d63066
ZNP8b <znp8b00ff@gmail.com>
2020-10-09 21:50:59 +0300

    using useRef instead getElement

--- src/components/MyComponent/index.tsx
+++ src/components/MyComponent/index.tsx
@@ -1,20 +1,17 @@
 import React, { useState, useRef } from "react";
 
 export const MyComponent: React.FC = () => {
-    let InputEl = useRef("");
+    const InputRef = useRef<HTMLInputElement | null>(null);
     const [value, setValue] = useState("");
     return (
-        <div
-            style={{
-                border: "1px solid silver"
-            }}
-        >
-            <input type="text" ref={InputEl.current} />
+        <div>
+            <input type="text" ref={InputRef} />
             <br/>
             {value}
             <br/>
             <button
-                onClick={() => setValue(InputEl.current)}
+            
+                onClick={() => {if (InputRef.current?.value != null) {setValue(InputRef.current?.value)}}}
             >
                 OK
             </button>
