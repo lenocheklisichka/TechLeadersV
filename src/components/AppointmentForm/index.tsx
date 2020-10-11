5b12bcd022377ed2e59766b5b6c4e0dd75404f7a
korytovaf <61353426+korytovaf@users.noreply.github.com>
2020-10-11 12:41:54 +0300

    initial commit

---
+++ src/components/AppointmentForm/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AppointmentForm = () => {
+
+
+    return (
+        <div>
+            form of an appointment
+        </div>
+    );
+}
+
+export default AppointmentForm
